c3e376ef8d07a836cce02337b50e65b9dcb7a15e
mikeaussie <michael.pyatin@me.com>
2016-02-28 14:15:47 +1000

    adding a new project

--- 2Bombs/ViewController.swift
+++ 2Bombs/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var showBothButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRButton(sender: UIButton) {
+        redBomb.hidden = true
+    }
+    @IBAction func hideBButton(sender: UIButton) {
+        blueBomb.hidden = true
+    }
+    @IBAction func showBombs(sender: AnyObject) {
+        blueBomb.hidden = false
+        redBomb.hidden = false
+    }
 
 }
 
